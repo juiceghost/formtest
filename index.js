fa0b06bead3e9ebfaeee8d53ae570987cb852bcc
juiceghost <jg@renegade.se>
2020-09-28 16:31:09 +0200

    Step one complete

--- index.js
+++ index.js
@@ -3,9 +3,13 @@
 const form = document.getElementsByTagName('form')[0];
 
 const email = document.getElementById('mail');
+
+const message = document.getElementById('message');
+const element = document.getElementById('messages');
+
 const emailError = document.querySelector('#mail + span.error');
 
-email.addEventListener('input', function (event) {
+/* email.addEventListener('input', function (event) {
     // Each time the user types something, we check if the
     // form fields are valid.
     if (email.validity.valid) {
@@ -17,20 +21,33 @@ email.addEventListener('input', function (event) {
         // If there is still an error, show the correct error
         showError();
     }
-});
+}); */
 
 form.addEventListener('submit', function (event) {
     // if the email field is valid, we let the form submit
     event.preventDefault();
+    const timestamp = Date.now();
+    const date = new Date(timestamp);
+    console.log(date.toLocaleString())
+    console.log(email.value)
+    console.log(message.value)
 
-    if (!email.validity.valid) {
-        // If it isn't, we display an appropriate error message
-        showError();
-        // Then we prevent the form from being sent by canceling the event
+    var tag = document.createElement("p");
+    //let messageBody = "On " + ---" +email.value + " wrote \"" + message.value + "\""
+    var text = document.createTextNode(`On ${date.toLocaleTimeString()}, ${email.value} wrote "${message.value}"`);
 
-    } else {
-        window.location.href = "http://127.0.0.1:5500/thankyou.html";
-    }
+    tag.appendChild(text);
+
+    element.appendChild(tag);
+
+    /*     if (!email.validity.valid) {
+            // If it isn't, we display an appropriate error message
+            showError();
+            // Then we prevent the form from being sent by canceling the event
+    
+        } else {
+            window.location.href = "http://127.0.0.1:5500/thankyou.html";
+        } */
 });
 
 function showError() {
